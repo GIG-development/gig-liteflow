407a611e23f528f6b03beb2820b2f2fd4567b7af
Maw <shinji.256@gmail.com>
2023-03-31 14:23:50 -0600

    New: Sign API for MoonPay integration

--- pages/api/mp/sign.ts
+++ pages/api/mp/sign.ts
@@ -3,9 +3,18 @@ import { NextApiRequest, NextApiResponse } from 'next'
 export default async function sign(
     req: NextApiRequest,
     res: NextApiResponse,
-  ): Promise<void> {
-    if(req)
+  ): Promise<any> {
+    console.log('test')
+    if(req){
         res
             .status(200)
-            .json({ test: 'Test', request: req })
+            .json({ test: 'Test' })
+    }
+    res.status(301).end()
+  }
+
+  export const config = {
+    api: {
+      bodyParser: false,
+    },
   }
