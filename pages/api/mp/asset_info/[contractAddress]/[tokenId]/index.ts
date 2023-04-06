493fcab89baa8b748886a5edd5efbc83028cc61c
Maw <shinji.256@gmail.com>
2023-04-06 17:54:40 -0600

    New: AssetInfo API for MoonPay integration

--- pages/api/mp/asset_info/[contractAddress]/[tokenId]/index.ts
+++ pages/api/mp/asset_info/[contractAddress]/[tokenId]/index.ts
@@ -93,8 +93,8 @@ const asset_info = async(
         
         const timestamp: (number|undefined) = req.query.timestamp ? Number(req.query.timestamp.toString()) : undefined
         const signature: (string|undefined) = req.query.signature ? req.query.signature.toString() : undefined
-        const path: (string|undefined) = req.url ? req.url.substring(7,req.url.length) : undefined
-        const method: (string|undefined) = req.method ? req.method : undefined
+        const path: (string|undefined) = req.url ? req.url.substring(7,req.url.length).split('&signature=')[0] : undefined
+        const method: (string|undefined) = req.method ? req.method.toUpperCase() : undefined
 
         if(timestamp && signature && path && method){
 
