f838940fa3bbd844bf9ae664da2c09167fb97564
Maw <shinji.256@gmail.com>
2023-04-17 17:36:33 -0600

    Nuevo: componente de Tooltip y nuevos tooltips en el proceso de creacion y wrap

--- components/Tooltip/Tooltip.tsx
+++ components/Tooltip/Tooltip.tsx
@@ -1,13 +1,11 @@
 import { Box, Tooltip as TT } from '@chakra-ui/react'
 import { FC, PropsWithChildren } from 'react'
-import { BsFillInfoCircleFill as InfoIcon2 } from '@react-icons/all-files/bs/BsFillInfoCircleFill'
+import { BsFillInfoCircleFill as InfoIcon } from '@react-icons/all-files/bs/BsFillInfoCircleFill'
 
 type Props = {
     text: string
 }
 
-const InfoIcon = <svg xmlns="http://www.w3.org/2000/svg" width='14px' height='14px' fill='lightgray' viewBox="0 0 512 512"><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-208a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg>
-  
 const Tooltip: FC<PropsWithChildren<Props>> = ({
     text
 }) => { 
@@ -24,8 +22,7 @@ const Tooltip: FC<PropsWithChildren<Props>> = ({
                 rounded={4}
             >
                 <span>
-                    {InfoIcon}
-                    <InfoIcon2 color='lightgray' fontSize={12}/>
+                    <InfoIcon color='lightgray' fontSize={12}/>
                 </span>
             </TT>
         </Box>
